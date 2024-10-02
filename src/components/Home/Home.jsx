45f95fa1867d1d6aa63ac067fc394654906c1946
Tushar Patle <116018292+tusharpatle0097@users.noreply.github.com>
2024-10-02 20:53:03 +0530

    remove button

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -46,12 +46,11 @@ const Home = () => {
             <span className='text-[18px] my-4 text-black]'>Proficient in HTML, CSS, JavaScript, React.js, and exploring Next.js, TypeScript. Specialize in crafting user-centric websites. Excited for collaborative challenges with professionals.</span>
           </div>
           <div className='mt-3'>
-            <button className={`bg-[#3fe2c4] ${isDarkMode ? "text-white" : "text-white"} flex rounded-lg px-3 py-2 lg:hover:scale-105 duration-200`}>
+            {/* <button className={`bg-[#3fe2c4] ${isDarkMode ? "text-white" : "text-white"} flex rounded-lg px-3 py-2 lg:hover:scale-105 duration-200`}>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-2" id="download-icon"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" x2="12" y1="15" y2="3"></line></svg>
-              {/* <span onClick={handleDownload}>Download Resume</span> */}
               <span>Download Resume</span>
 
-            </button>
+            </button> */}
           </div>
         </div>
         <div className='lg:mt-[4rem] mt-[3rem]'>
